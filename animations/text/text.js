a5762d38e9707e5dcb6e3c882d1406abc67926e9
Torin Etheridge <torinriley220@gmail.com>
2024-10-08 11:36:16 -0400

    added text animation

---
+++ animations/text/text.js
@@ -0,0 +1,34 @@
+const scrambleText = (element, text) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-[]{}|;:",.<>?/`~';
+    let iterations = 0;
+
+    
+    element.innerText = text
+        .split('')
+        .map(() => characters[Math.floor(Math.random() * characters.length)])
+        .join('');
+
+    const interval = setInterval(() => {
+        element.innerText = text
+            .split('')
+            .map((char, index) => {
+                if (index < iterations) {
+
+                }
+               
+                return characters[Math.floor(Math.random() * characters.length)];
+            })
+            .join('');
+
+        
+        if (iterations >= text.length) {
+            clearInterval(interval);  
+        }
+        iterations += 1 / 3; 
+    }, 50);  
+};
+
+
+const targetText = 'Decrypting...'; 
+const textElement = document.getElementById('scramble-text');
+scrambleText(textElement, targetText);
